a867d2c8017365b99fd495fdb5baa681ce9ff8e0
Chris Alberty <17458734+Vrykodin@users.noreply.github.com>
2022-03-09 21:43:06 -0600

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +14,45 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet( "id:int", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            
+            if ( celestialObject == null)
+                return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects
+                .Where(e => e.OrbitedObjectId == id)
+                .ToList();
+            return Ok( celestialObject );
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName( string name )
+        {
+            var celestialObject = _context.CelestialObjects.Find(name);
+            if( celestialObject == null )
+                return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects
+                .Where(e => e.OrbitedObjectId == celestialObject.Id)
+                .ToList();
+            return Ok( celestialObject );
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects;
+
+            foreach( var celestialObject in celestialObjects )
+            {
+                celestialObject.Satellites = _context.CelestialObjects
+                .Where( e => e.OrbitedObjectId == celestialObject.Id )
+                .ToList();
+            }
+            return Ok(celestialObjects);
         }
     }
 }
