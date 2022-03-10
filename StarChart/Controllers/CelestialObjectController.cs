6fdceb127fe64b7902bd3e73171596927d271c95
Chris Alberty <17458734+Vrykodin@users.noreply.github.com>
2022-03-09 21:51:18 -0600

    Changes

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,7 +16,7 @@ namespace StarChart.Controllers
             _context = context;
         }
 
-        [HttpGet( "id:int", Name = "GetById")]
+        [HttpGet("{id:int}", Name = "GetById")]
         public IActionResult GetById(int id)
         {
             var celestialObject = _context.CelestialObjects.Find(id);
@@ -30,15 +30,21 @@ namespace StarChart.Controllers
         }
 
         [HttpGet("{name}")]
-        public IActionResult GetByName( string name )
+        public IActionResult GetByName(string name)
         {
-            var celestialObject = _context.CelestialObjects.Find(name);
-            if( celestialObject == null )
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name);
+
+            if( !celestialObjects.Any() )
                 return NotFound();
-            celestialObject.Satellites = _context.CelestialObjects
-                .Where(e => e.OrbitedObjectId == celestialObject.Id)
-                .ToList();
-            return Ok( celestialObject );
+
+            foreach( var celestialObject in celestialObjects )
+            {
+                celestialObject.Satellites = _context.CelestialObjects
+                    .Where( e => e.OrbitedObjectId == celestialObject.Id )
+                    .ToList();
+            }
+            
+            return Ok(celestialObjects.ToList());
         }
 
         [HttpGet]
